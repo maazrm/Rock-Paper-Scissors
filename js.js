99dd8fda6dc24f8d9fce3747cdcfbcf3cfdbdb55
Maaz Rashid Mateen <maaz.r2001@gmail.com>
2022-06-27 19:32:00 +0300

    Made function for computer selection
    Made a computerPlay function so computer can randomly select a choice

---
+++ js.js
@@ -0,0 +1,11 @@
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissor']
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+
+    return (randomChoice)
+}
+
+
+
+
+
